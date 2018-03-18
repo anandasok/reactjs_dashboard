f79539dd4fc273dd33bff19edc1204e29b543b37
Anandraj <anandasok5@gmail.com>
2018-03-18 14:28:06 +0530

    Dashboard: Side nav bar menu code refactor.

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,24 +1,98 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
+import Dashboard from './Dashboard';
+import Reports from './Reports';
+import Archive from './Archive';
+import Social from './Social';
+import Users from './Users';
+import Documents from './Documents';
+import Favorites from './Favorites';
+import Tools from './Tools';
+import Settings from './Settings';
 import '../css/SideNavbar.css';
 
 class SideMenu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuItems: [
+                {name: 'Dashboard', icon: 'dashboard'},
+                {name: 'Reports', icon: 'stats'},
+                {name: 'Archive', icon: 'eye-open'},
+                {name: 'Social', icon: 'tint'},
+                {name: 'Users', icon: 'user'},
+                {name: 'Documents', icon: 'duplicate'},
+                {name: 'Favorites', icon: 'star'},
+                {name: 'Tools', icon: 'wrench'},
+                {name: 'Settings', icon: 'cog'}
+            ]
+        }
+    }
+
+    listItem = (item, index) => {
+        let name = item.name;
+        let cls = name.toLowerCase() === 'dashboard' ? ' active' : '';
+        let liElement = <li key={index} onClick={this.handleMenuClick} className={cls} data-id={item.name}>
+            <a><span className={`glyphicon glyphicon-${item.icon}`}> </span>{item.name}</a>
+        </li>;
+
+        return liElement;
+    };
 
     handleMenuClick = (event) => {
 
         event.preventDefault();
 
-        let ul = document.getElementById("menu-list");
-        let items = ul.getElementsByTagName("li");
+        let ul = document.getElementById('menu-list');
+        let items = ul.getElementsByTagName('li');
         let element;
         for (let i = 0; i < items.length; ++i) {
             element = items[i];
-            // element.classList.remove("active");
-            element.removeAttribute("class");
+            // element.classList.remove('active');
+            element.removeAttribute('class');
         }
 
         let currentElement = event.currentTarget;
-        currentElement.className = currentElement.className + " active";
+        currentElement.className = currentElement.className + ' active';
+
+        let currentComponentId = currentElement.dataset.id.toLowerCase();
+        let currentComponent = null;
+        switch (currentComponentId) {
+            case 'dashboard':
+                currentComponent = <Dashboard/>;
+                break;
+            case 'reports':
+                currentComponent = <Reports/>;
+                break;
+            case 'archive':
+                currentComponent = <Archive/>;
+                break;
+            case 'social':
+                currentComponent = <Social/>;
+                break;
+            case 'users':
+                currentComponent = <Users/>;
+                break;
+            case 'documents':
+                currentComponent = <Documents/>;
+                break;
+            case 'favorites':
+                currentComponent = <Favorites/>;
+                break;
+            case 'tools':
+                currentComponent = <Tools/>;
+                break;
+            case 'settings':
+                currentComponent = <Settings/>;
+                break;
+            default:
+                break;
+
+        }
+
+        ReactDOM.render(currentComponent, document.getElementById('content-area'));
 
     };
 
@@ -35,33 +109,9 @@ class SideMenu extends Component {
                     </div>
                     <div id="side-navbar-menu" className="navbar-collapse collapse">
                         <ul id="menu-list" className="nav navbar-nav">
-                            <li onClick={this.handleMenuClick} className="active">
-                                <a><span className="glyphicon glyphicon-dashboard"></span> Dashboard</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-stats"></span> Reports</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-eye-open"></span> Archive</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-tint"></span> Social</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-user"></span> Users</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-duplicate"></span> Documents</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-star"></span> Favorites</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-wrench"></span> Tools</a>
-                            </li>
-                            <li onClick={this.handleMenuClick}>
-                                <a><span className="glyphicon glyphicon-cog"></span> Settings</a>
-                            </li>
+                            {
+                                this.state.menuItems.map(this.listItem)
+                            }
                         </ul>
 
                         <form className="navbar-form" role="search">
